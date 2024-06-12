4a91c2f6792bf536f4c4b057da27e73f5d2ce1eb
Roberto Andree Urraca Serrano <roberto.urraca@atos.net>
2024-06-12 13:42:23 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <h1>Spotify Clone</h1>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { StateProvider } from './utils/StateProvider.tsx'
+import reducer, {initialState} from './utils/reducer.js'
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <StateProvider  initialState={initialState} reducer={reducer}>
+      <App />
+    </StateProvider>
+  </React.StrictMode>,
+)
